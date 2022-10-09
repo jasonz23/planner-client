079805d374b034a8ef7fdde56ce5bc621a7ebd82
Jason Zhao <jason.zhao@stnvideo.com>
2022-10-09 01:34:21 -0700

    imported tasks and calendar

--- src/pages/main-page/index.jsx
+++ src/pages/main-page/index.jsx
@@ -1,10 +1,16 @@
 import react from "react";
+import Calendar from "../../components/calendar";
 import NavBar from "../../components/nav-bar";
+import Tasks from "../../components/tasks";
 
 const MainPage = () => {
   return (
     <div>
       <NavBar />
+      <div>
+        <Tasks />
+        <Calendar />
+      </div>
     </div>
   );
 };
