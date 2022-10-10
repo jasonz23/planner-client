ac2ec1148d7fdd372d8f141956912b3285f52c09
Jason Zhao <jason.zhao@stnvideo.com>
2022-10-09 21:10:08 -0700

    added current date to tasks component

--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import { JsxElement } from "typescript";
 import { tasks } from "../../slices/tasks";
+import "./tasks.css";
 
 const Tasks = () => {
-  return <div></div>;
+  const currentMonth = (): string => {
+    return new Date().toLocaleString("en-US", { month: "long" });
+  };
+
+  const currentDay = (): string => {
+    return new Date().toLocaleString("en-US", { day: "2-digit" });
+  };
+
+  const getTasks = (): ReactNode | null => {
+    return <h1>COW</h1>;
+  };
+
+  const getSubtitle = (): ReactNode | null => {
+    return <h2>Tasks etc</h2>;
+  };
+
+  return (
+    <div>
+      <div>
+        <h1 className="current-date-title">
+          {currentMonth()} {currentDay()}
+        </h1>
+      </div>
+      <div>{getSubtitle()}</div>
+      {getTasks()}
+    </div>
+  );
 };
 
 export default Tasks;
