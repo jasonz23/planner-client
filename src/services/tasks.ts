6f5da09131fe80e59cb2e2049960091dc91ffa31
Jason Zhao <jason.zhao@stnvideo.com>
2022-10-10 23:25:47 -0700

    added functions that will need to finish in the future

--- src/services/tasks.ts
+++ src/services/tasks.ts
@@ -1,1 +1,10 @@
 import react from "react";
+
+export const getTasks = () => {}
+
+export const getTask = (id: number) => {}
+
+export const getFilteredTasks = (filter: string) => {}
+
+export const getTasksWithDate = (date: string) => {}
+
