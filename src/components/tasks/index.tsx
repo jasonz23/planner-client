1aab3fcbc2c684db86397838aca64c3cfdbec87b
Jason Zhao <jason1382311@gmail.com>
2022-11-06 18:20:22 -0800

    ui changes and added values from redux, add Add Task button

--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -1,10 +1,14 @@
-import React, { ReactNode } from "react";
-import { JsxElement } from "typescript";
-import { tasks } from "../../slices/tasks";
+import React, { ReactNode, useState } from "react";
 import Task from "../task";
+import ControlPointOutlinedIcon from "@mui/icons-material/ControlPointOutlined";
 import "./tasks.css";
+import AddTaskModal from "../add-task-modal";
+import { RootState } from "../../slices/reducers";
+import { useSelector } from "react-redux";
 
 const Tasks = () => {
+  const [showAddTaskModal, setShowAddTaskModal] = useState(false);
+  const tasks = useSelector((state: RootState) => state.tasks.tasksList);
   const currentMonth = (): string => {
     return new Date().toLocaleString("en-US", { month: "long" });
   };
@@ -14,23 +18,43 @@ const Tasks = () => {
   };
 
   const getTasks = (): ReactNode | null => {
-    return <Task />;
+    return tasks?.map((task: any, index: number) => {
+      return <Task data={task} key={index} />;
+    });
   };
 
   const getSubtitle = (): ReactNode | null => {
-    return <h2>Tasks etc</h2>;
+    return (
+      <h2>
+        {tasks.length} {tasks.length > 1 ? "Tasks" : "Task"}
+      </h2>
+    );
   };
 
   return (
-    <div style={{ flex: 1.5, marginRight: "10px" }}>
-      <div>
-        <h1 className="current-date-title">
-          {currentMonth()} {currentDay()}
-        </h1>
+    <>
+      <AddTaskModal
+        show={showAddTaskModal}
+        setShow={(bool: boolean) => setShowAddTaskModal(bool)}
+      />
+      <div style={{ flex: 1.5, marginRight: "10px" }}>
+        <div>
+          <h1 className="current-date-title">
+            {currentMonth()} {currentDay()}
+          </h1>
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          {getSubtitle()}
+          <ControlPointOutlinedIcon
+            style={{ color: "blue", marginLeft: "10px", cursor: "pointer" }}
+            onClick={() => {
+              setShowAddTaskModal(!showAddTaskModal);
+            }}
+          />
+        </div>
+        {getTasks()}
       </div>
-      <div>{getSubtitle()}</div>
-      {getTasks()}
-    </div>
+    </>
   );
 };
 
