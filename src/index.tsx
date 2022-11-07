ca8e3d1c78074b86464f1d24fcfdaf2139042d96
Jason Zhao <jason1382311@gmail.com>
2022-11-06 18:18:53 -0800

    injected redux into app

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./slices";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div className="root">
-      <App />
-    </div>
+    <Provider store={store}>
+      <div className="root">
+        <App />
+      </div>
+    </Provider>
   </React.StrictMode>
 );
 
