d2134f33c912962bd02ef17ebbf96c2987963ac5
Jason Zhao <jason1382311@gmail.com>
2022-11-06 18:21:57 -0800

    add AddTask reducer and states

--- src/slices/tasks.ts
+++ src/slices/tasks.ts
@@ -1,18 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
+interface TasksState {
+    tasksList: any;
+}
 
 const initialState = {
     tasksList: [],
-    tasksListLength: 0,
-    tasksFilteredByDate: [],
 
-}
+} as TasksState
 
 const slice = createSlice({
     name: "tasks",
     initialState,
-    reducers:{}
-
+    reducers:{
+        addSingleTask: (state, {payload}: PayloadAction<object>)=> {
+            state.tasksList.push(payload);
+        }
+    }
 })
+
+export const {addSingleTask} = slice.actions;
 
 export const tasks = slice.reducer;
